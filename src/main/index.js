5572ee7331816fcbbf617e3833a9d476fd2afe83
realjema <realjema@gmail.com>
2020-11-09 13:11:04 +0100

    code cleanup

--- src/main/index.js
+++ src/main/index.js
@@ -1,7 +1,6 @@
 import path from 'path';
 import {app, BrowserWindow} from 'electron';
 
-// 加载本地入口页面地址，开发环境下用devServer，所以用的http协议；生产模式下用file协议
 const entryUrl = process.env.NODE_ENV === 'development'
   ? 'http://localhost:8080/index.html'
   : `file://${path.join(__dirname, 'index.html')}`;
@@ -9,7 +8,7 @@ const entryUrl = process.env.NODE_ENV === 'development'
 let window = null;
 
 app.on('ready', () => {
-  window = new BrowserWindow({ width: 500, height: 250, transparent: true, frame: false, resizable: true });
+  window = new BrowserWindow({ width: 700, height: 500, resizable: true });
   window.loadURL(entryUrl);
   window.on('closed', () => window = null);
 });
--- src/renderer/App.js
+++ src/renderer/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import AnalogClock from 'analog-clock-react';
 
 import './assets/styles.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -11,64 +10,13 @@ export class App extends React.PureComponent {
 	constructor(props) {
 		super(props);
 		this.state = {
-			bg: 'bg',
-			cb: 'containerbg',
 		};
-	}
-
-	handleChange(event) {
-		let value = event.target.value;
-		console.log(value);
-		this.setState({
-			bg: event.target.value,
-		});
-	}
-	closeWindow() {
-		const remote = window.require ? window.require('electron').remote : null;
-		const WIN = remote.getCurrentWindow();
-		WIN.close();
-	}
 
 	render() {
-		let options = {
-			width: '300px',
-			border: true,
-			borderColor: '##fff0',
-			baseColor: '##fff0',
-			centerColor: '#d81c7a',
-			centerBorderColor: '#fff',
-			handColors: {
-				second: '#d81c7a',
-				minute: '#d81c7a',
-				hour: '#d81c7a',
-			},
-		};
 		return (
-			<Container style={{ backgroundImage: 'url(./assets/images/' + this.state.cb + '.png)' }}>
-				<Row>
-					<Col>
-						<Draggable>
-							<div
-								className="theClock"
-								style={{ backgroundImage: 'url(./assets/images/' + this.state.bg + '.png)' }}
-							>
-								<AnalogClock {...options} />
-							</div>
-						</Draggable>
-					</Col>
-					<Col className="controls">
-							<h5>Controls</h5>
-							<button onClick={this.closeWindow}>Close</button>
-							<select onChange={this.handleChange.bind(this)}>
-								<option value="bg">Clock 1</option>
-								<option value="bg2">Clock 2</option>
-								<option value="bg3">Clock 3</option>
-								<option value="bg4">Clock 4</option>
-								<option value="bg5">Clock 5</option>
-							</select>
-					</Col>
-				</Row>
-			</Container>
+			<div>
+				<h1>Hello World</h1>
+			</div>
 		);
 	}
 }
