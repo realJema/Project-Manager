a75e3b6dec67d74a1c8214d9b4cead52bff08611
realjema <realjema@gmail.com>
2020-11-09 13:49:27 +0100

    minor changes

--- src/renderer/App.js
+++ src/renderer/App.js
@@ -2,62 +2,46 @@ import React, { Component } from "react";
 import CheckList from "./Components/checklist";
 import marked from "marked";
 
-class App extends Component {
-  constructor() {
-    //   ...arguments stores values of varying lengths
-    super(...arguments);
-    this.state = {
-      showDetails: true,
-    };
-  }
+export class App extends React.PureComponent {
+	constructor() {
+		//   ...arguments stores values of varying lengths
+		super(...arguments);
+		this.state = {
+			showDetails: true,
+		};
+	}
 
-  //   function definitions
-  toggleDetails() {
-    this.setState({ showDetails: !this.state.showDetails });
-  }
+	//   function definitions
+	toggleDetails() {
+		this.setState({ showDetails: !this.state.showDetails });
+	}
 
-  render() {
-    let cardDetails;
-    if (this.state.showDetails) {
-      cardDetails = (
-        <div className="card__details">
-          <span  contentEditable="true"
-            dangerouslySetInnerHTML={{ __html: marked(this.props.description) }}
-          />
-          <CheckList cardId={this.props.id} tasks={this.props.tasks} />
-        </div>
-      );
-    }
+	render() {
+		let cardDetails;
+		if (this.state.showDetails) {
+			cardDetails = (
+				<div className="card__details">
+					<span contentEditable="true" dangerouslySetInnerHTML={{ __html: marked(this.props.description) }} />
+					<CheckList cardId={this.props.id} tasks={this.props.tasks} />
+				</div>
+			);
+		}
 
-    // inline styling
-    let sideColor = {
-      position: "absolute",
-      zIndex: -1,
-      top: 0,
-      bottom: 0,
-      left: 0,
-      width: 7,
-      backgroundColor: this.props.color,
-    };
+		// inline styling
+		let sideColor = {
+			position: 'absolute',
+			zIndex: -1,
+			top: 0,
+			bottom: 0,
+			left: 0,
+			width: 7,
+			backgroundColor: this.props.color,
+		};
 
-    return (
-      <div className="card">
-        <div style={sideColor} />
-        <div
-          className={
-            this.state.showDetails
-              ? "card__title card__title--is-open"
-              : "card__title"
-          }
-          onClick={this.toggleDetails.bind(this)}
-        >
-          {this.props.title}
-        </div>
-        {cardDetails}
-      </div>
-    );
-  }
+		return (
+			<div className="card">
+				<h1>Hello </h1>
+			</div>
+		);
+	}
 };
-
-
-export default App;
