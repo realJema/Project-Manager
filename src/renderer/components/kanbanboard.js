86af466925b2354e0ca0c340936a0036e3223063
realjema <realjema@gmail.com>
2021-03-18 13:03:09 +0100

    cards moving from one list to another but reordering not working

--- src/renderer/components/kanbanboard.js
+++ src/renderer/components/kanbanboard.js
@@ -1,20 +1,232 @@
 import React, { Component } from 'react';
-import List from './list';
+import Card from './card';
+import { PlusSquare } from 'react-bootstrap-icons';
+import { useSetRecoilState } from 'recoil';
+import { todoListState } from './globalState';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
-import { Row } from 'react-bootstrap';
+import { Col, Row } from 'react-bootstrap';
 
-function KanbanBoard(props) {
+const grid = 8;
+
+const getItemStyle = (isDragging, draggableStyle) => ({
+	// some basic styles to make the items look a bit nicer
+	userSelect: 'none',
+	padding: grid * 2,
+	margin: `0 0 ${grid}px 0`,
+
+	// change background colour if dragging
+	background: isDragging ? 'lightgreen' : 'grey',
+
+	// styles we need to apply on draggables
+	...draggableStyle,
+});
+
+const getListStyle = (isDraggingOver) => ({
+	background: isDraggingOver ? 'lightblue' : 'lightgrey',
+	padding: grid,
+	width: 250,
+});
+
+// const setTodoList = useSetRecoilState(todoListState);
+
+// a little function to help us with reordering the result
+const reorder = (list, startIndex, endIndex) => {
+	const result = Array.from(list);
+	const [removed] = result.splice(startIndex, 1);
+	result.splice(endIndex, 0, removed);
+
+	return result;
+};
+
+class KanbanBoard extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			items: this.props.cards,
+		};
+		this.onDragEnd = this.onDragEnd.bind(this);
+	}
+
+	handleAddCard(title) {
+		console.log(title.replace(/\s+/g, '-').toLowerCase());
+		setTodoList((oldTodoList) => [
+			...oldTodoList,
+			{
+				id: oldTodoList.length + 1,
+				title: 'Enter New Task',
+				color: '',
+				description: '',
+				status: title.replace(/\s+/g, '-').toLowerCase(),
+				tasks: [],
+			},
+		]);
+	}
+
+	onDragEnd(result) {
+		console.log(this.state.items);
+		const { source, destination } = result;
+		// dropped outside the list
+		if (!result.destination) {
+			return;
+		}
+		
+		const items = reorder(this.state.items, source.index, destination.index);
+		
+		console.log(items);
+		this.setState({
+			items,
+		});
+		console.log(this.state.items);
+		console.log('droppableId ____________________');
+		console.log('Source Id: ' + source.droppableId);
+		console.log('Destination Id: ' + destination.droppableId);
+		console.log('index ____________________');
+		console.log('Source Id: ' + source.index);
+		console.log('Destination Id: ' + destination.index);
+	}
+
+	render() {
 		return (
-			<Row>
-				<List id="todo" title="To Do" cards={props.cards.filter((card) => card.status === 'to-do')} />
-				<List
-					id="in-progress"
-					title="In Progress"
-					cards={props.cards.filter((card) => card.status === 'in-progress')}
-				/>
-				<List id="done" title="Done" cards={props.cards.filter((card) => card.status === 'done')} />
-			</Row>
+			<DragDropContext onDragEnd={this.onDragEnd}>
+				<Droppable droppableId="todo">
+					{(provided, snapshot) => (
+						<Col
+							xs={12}
+							sm={12}
+							md={4}
+							id="To Do"
+							className="list"
+							style={getListStyle(snapshot.isDraggingOver)}
+							ref={provided.innerRef}
+						>
+							<Row>
+								<h1 className="kanban-headers">{this.props.title}</h1>
+								<PlusSquare
+									className="ml-4 kanban-list-plus"
+									onClick={(event) => handleAddCard(this.props.title)}
+								/>
+							</Row>
+
+							{this.state.items
+								.filter((card) => card.status === 'to-do')
+								.map((card, index) => (
+									<Draggable key={card.id.toString()} draggableId={card.id.toString()} index={index}>
+										{(provided, snapshot) => (
+											<div
+												ref={provided.innerRef}
+												{...provided.draggableProps}
+												{...provided.dragHandleProps}
+											>
+												<Card
+													key={card.id}
+													id={card.id}
+													title={card.title}
+													color={card.color}
+													description={card.description}
+													tasks={card.tasks}
+												/>
+											</div>
+										)}
+									</Draggable>
+								))}
+							{provided.placeholder}
+						</Col>
+					)}
+				</Droppable>
+				<Droppable droppableId="in-progress">
+					{(provided, snapshot) => (
+						<Col
+							xs={12}
+							sm={12}
+							md={4}
+							id="In Progress"
+							className="list"
+							style={getListStyle(snapshot.isDraggingOver)}
+							ref={provided.innerRef}
+						>
+							<Row>
+								<h1 className="kanban-headers">{this.props.title}</h1>
+								<PlusSquare
+									className="ml-4 kanban-list-plus"
+									onClick={(event) => handleAddCard(this.props.title)}
+								/>
+							</Row>
+
+							{this.state.items
+								.filter((card) => card.status === 'in-progress')
+								.map((card, index) => (
+									<Draggable key={card.id.toString()} draggableId={card.id.toString()} index={index}>
+										{(provided, snapshot) => (
+											<div
+												ref={provided.innerRef}
+												{...provided.draggableProps}
+												{...provided.dragHandleProps}
+											>
+												<Card
+													key={card.id}
+													id={card.id}
+													title={card.title}
+													color={card.color}
+													description={card.description}
+													tasks={card.tasks}
+												/>
+											</div>
+										)}
+									</Draggable>
+								))}
+							{provided.placeholder}
+						</Col>
+					)}
+				</Droppable>
+				<Droppable droppableId="done">
+					{(provided, snapshot) => (
+						<Col
+							xs={12}
+							sm={12}
+							md={4}
+							id="Done"
+							className="list"
+							style={getListStyle(snapshot.isDraggingOver)}
+							ref={provided.innerRef}
+						>
+							<Row>
+								<h1 className="kanban-headers">{this.props.title}</h1>
+								<PlusSquare
+									className="ml-4 kanban-list-plus"
+									onClick={(event) => handleAddCard(this.props.title)}
+								/>
+							</Row>
+
+							{this.state.items
+								.filter((card) => card.status === 'done')
+								.map((card, index) => (
+									<Draggable key={card.id.toString()} draggableId={card.id.toString()} index={index}>
+										{(provided, snapshot) => (
+											<div
+												ref={provided.innerRef}
+												{...provided.draggableProps}
+												{...provided.dragHandleProps}
+											>
+												<Card
+													key={card.id}
+													id={card.id}
+													title={card.title}
+													color={card.color}
+													description={card.description}
+													tasks={card.tasks}
+												/>
+											</div>
+										)}
+									</Draggable>
+								))}
+							{provided.placeholder}
+						</Col>
+					)}
+				</Droppable>
+			</DragDropContext>
 		);
+	}
 }
 
 export default KanbanBoard;
--- src/renderer/components/list.js
+++ src/renderer/components/list.js
@@ -7,49 +7,46 @@ import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 import { Col, Row } from 'react-bootstrap';
 
-
 const grid = 8;
 
 const getItemStyle = (isDragging, draggableStyle) => ({
-    // some basic styles to make the items look a bit nicer
-    userSelect: 'none',
-    padding: grid * 2,
-    margin: `0 0 ${grid}px 0`,
+	// some basic styles to make the items look a bit nicer
+	userSelect: 'none',
+	padding: grid * 2,
+	margin: `0 0 ${grid}px 0`,
 
-    // change background colour if dragging
-    background: isDragging ? 'lightgreen' : 'grey',
+	// change background colour if dragging
+	background: isDragging ? 'lightgreen' : 'grey',
 
-    // styles we need to apply on draggables
-    ...draggableStyle
+	// styles we need to apply on draggables
+	...draggableStyle,
 });
 
-const getListStyle = isDraggingOver => ({
-    background: isDraggingOver ? 'lightblue' : 'lightgrey',
-    padding: grid,
-    width: 250
+const getListStyle = (isDraggingOver) => ({
+	background: isDraggingOver ? 'lightblue' : 'lightgrey',
+	padding: grid,
+	width: 250,
 });
-
 
 // const setTodoList = useSetRecoilState(todoListState);
 
 // a little function to help us with reordering the result
 const reorder = (list, startIndex, endIndex) => {
-  const result = Array.from(list);
-  const [removed] = result.splice(startIndex, 1);
-  result.splice(endIndex, 0, removed);
+	const result = Array.from(list);
+	const [removed] = result.splice(startIndex, 1);
+	result.splice(endIndex, 0, removed);
 
-  return result;
+	return result;
 };
 
 class List extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			items: this.props.cards
+			items: this.props.cards,
 		};
 		this.onDragEnd = this.onDragEnd.bind(this);
 	}
-
 
 	handleAddCard(title) {
 		console.log(title.replace(/\s+/g, '-').toLowerCase());
@@ -64,82 +61,162 @@ class List extends Component {
 				tasks: [],
 			},
 		]);
-	};
+	}
 
 	onDragEnd(result) {
-			
-        const { source, destination } = result;
-		 // dropped outside the list
+		const { source, destination } = result;
+		// dropped outside the list
 		if (!result.destination) {
 			return;
 		}
-		
-		const items = reorder(
-			this.state.items,
-			result.source.index,
-			result.destination.index
-		);
+
+		const items = reorder(this.state.items, result.source.index, result.destination.index);
 
 		this.setState({
-			items
+			items,
 		});
-		console.log("after ____________________");
-		console.log("Source Id " + source.droppableId + "/nDestination Id" + destination.droppableId);
+		console.log('after ____________________');
+		console.log('Source Id ' + source.droppableId + '/nDestination Id' + destination.droppableId);
 	}
 
 	render() {
-		
 		return (
 			<DragDropContext onDragEnd={this.onDragEnd}>
-				<Col
-					xs={12} 
-					sm={12} 
-					md={4} 
-					id={this.props.title}
-					className="list" >
-					<Row>
-						<h1 className="kanban-headers">{this.props.title}</h1>
-						<PlusSquare
-							className="ml-4 kanban-list-plus"
-							onClick={(event) => handleAddCard(this.props.title)}
-						/>
-					</Row>
-
-				<Droppable droppableId={this.props.title} >
+				<Droppable droppableId="todo">
 					{(provided, snapshot) => (
-						<div 
+						<Col
+							xs={12}
+							sm={12}
+							md={4}
+							id="To Do"
+							className="list"
 							style={getListStyle(snapshot.isDraggingOver)}
-							ref={provided.innerRef} >
-								
-								{this.state.items.map((card, index) => (
-									<Draggable 
-										key={card.id.toString()}  
-										draggableId={card.id.toString()} 
-										index={index}
-									>
-											{(provided, snapshot) => (
-												<div
-													ref={provided.innerRef}
-													{...provided.draggableProps}
-													{...provided.dragHandleProps}
-												>
-													<Card
-														key={card.id}
-														id={card.id}
-														title={card.title}
-														color={card.color}
-														description={card.description}
-														tasks={card.tasks}
-													/>
-												</div>
-											)}
+							ref={provided.innerRef}
+						>
+							<Row>
+								<h1 className="kanban-headers">{this.props.title}</h1>
+								<PlusSquare
+									className="ml-4 kanban-list-plus"
+									onClick={(event) => handleAddCard(this.props.title)}
+								/>
+							</Row>
+
+							{this.state.items
+								.filter((card) => card.status === 'to-do')
+								.map((card, index) => (
+									<Draggable key={card.id.toString()} draggableId={card.id.toString()} index={index}>
+										{(provided, snapshot) => (
+											<div
+												ref={provided.innerRef}
+												{...provided.draggableProps}
+												{...provided.dragHandleProps}
+											>
+												<Card
+													key={card.id}
+													id={card.id}
+													title={card.title}
+													color={card.color}
+													description={card.description}
+													tasks={card.tasks}
+												/>
+											</div>
+										)}
 									</Draggable>
 								))}
-								{provided.placeholder}
-								</div>
+							{provided.placeholder}
+						</Col>
 					)}
 				</Droppable>
+				<Droppable droppableId="in-progress">
+					{(provided, snapshot) => (
+						<Col
+							xs={12}
+							sm={12}
+							md={4}
+							id="In Progress"
+							className="list"
+							style={getListStyle(snapshot.isDraggingOver)}
+							ref={provided.innerRef}
+						>
+							<Row>
+								<h1 className="kanban-headers">{this.props.title}</h1>
+								<PlusSquare
+									className="ml-4 kanban-list-plus"
+									onClick={(event) => handleAddCard(this.props.title)}
+								/>
+							</Row>
+
+							{this.state.items
+								.filter((card) => card.status === 'in-progress')
+								.map((card, index) => (
+									<Draggable key={card.id.toString()} draggableId={card.id.toString()} index={index}>
+										{(provided, snapshot) => (
+											<div
+												ref={provided.innerRef}
+												{...provided.draggableProps}
+												{...provided.dragHandleProps}
+											>
+												<Card
+													key={card.id}
+													id={card.id}
+													title={card.title}
+													color={card.color}
+													description={card.description}
+													tasks={card.tasks}
+												/>
+											</div>
+										)}
+									</Draggable>
+								))}
+							{provided.placeholder}
 						</Col>
+					)}
+				</Droppable>
+				<Droppable droppableId="done">
+					{(provided, snapshot) => (
+						<Col
+							xs={12}
+							sm={12}
+							md={4}
+							id="Done"
+							className="list"
+							style={getListStyle(snapshot.isDraggingOver)}
+							ref={provided.innerRef}
+						>
+							<Row>
+								<h1 className="kanban-headers">{this.props.title}</h1>
+								<PlusSquare
+									className="ml-4 kanban-list-plus"
+									onClick={(event) => handleAddCard(this.props.title)}
+								/>
+							</Row>
+
+							{this.state.items
+								.filter((card) => card.status === 'done')
+								.map((card, index) => (
+									<Draggable key={card.id.toString()} draggableId={card.id.toString()} index={index}>
+										{(provided, snapshot) => (
+											<div
+												ref={provided.innerRef}
+												{...provided.draggableProps}
+												{...provided.dragHandleProps}
+											>
+												<Card
+													key={card.id}
+													id={card.id}
+													title={card.title}
+													color={card.color}
+													description={card.description}
+													tasks={card.tasks}
+												/>
+											</div>
+										)}
+									</Draggable>
+								))}
+							{provided.placeholder}
+						</Col>
+					)}
+				</Droppable>
 			</DragDropContext>
 		);
 	}
